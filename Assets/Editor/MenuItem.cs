bce419caccf29cdb26e67804975e6c61ff452725
d00605132 <dingxiaowei2@huawei.com>
2022-04-30 23:13:03 +0800

    菜单栏添加函数分析快捷键(目前调用bat还有问题)

--- Assets/Editor/MenuItem.cs
+++ Assets/Editor/MenuItem.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,5 +9,20 @@ public static class MenuItems
     private static void ViewDownload()
     {
         EditorUtility.OpenWithDefaultApp(Application.persistentDataPath);
+    }
+
+    [MenuItem("MonitorTool/º¯Êý¹æ·¶ÐÔ¼ì²â")]
+    private static void ProcessExcuteFunctionCheck()
+    {
+        var bat = Application.dataPath.Replace("/Assets", "") + "/Tools/net6.0/run.bat";
+        UnityEngine.Debug.Log(bat);
+        ProcessStartInfo proc = new ProcessStartInfo("cmd.exe", bat);
+        proc.WindowStyle = ProcessWindowStyle.Normal;
+        //proc.CreateNoWindow = false;
+        //proc.UseShellExecute = true;
+        //proc.RedirectStandardError = false;
+        //proc.RedirectStandardInput = false;
+        //proc.RedirectStandardOutput = false;
+        Process.Start(proc);
     }
 }
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+Tools\net6.0\Linty.CLI.exe Assembly-CSharp.csproj
+@echo "生成完成"
+pause
