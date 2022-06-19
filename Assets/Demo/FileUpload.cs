b4af62da1f0243b81994a9505356bd76a64ad08b
d00605132 <dingxiaowei2@huawei.com>
2022-06-19 21:46:20 +0800

    测试HTTP文件上传

--- Assets/Demo/FileUpload.cs
+++ Assets/Demo/FileUpload.cs
@@ -6,24 +6,24 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class FileUpload : MonoBehaviour
-{
-    private void Start()
-    {
-        var uploadManager = gameObject.AddComponent<FileUploadManager>();
-        uploadManager.UploadFiles(Config.PostFileHeaders, new System.Collections.Generic.Dictionary<string, string>() { { "testcase_name", "testaladdin1" } }, "folder", new System.Collections.Generic.List<string>() { { "D:/captureFrame_2022_5_1_23_10_50.zip" } }, (res, errorInfo) =>
-        {
-            Debug.Log($"传输结果:{res}  error:{errorInfo}");
-        });
-    }
-}
+//public class FileUpload : MonoBehaviour
+//{
+//    private void Start()
+//    {
+//        var uploadManager = gameObject.AddComponent<FileUploadManager>();
+//        uploadManager.UploadFiles(Config.PostFileHeaders, new System.Collections.Generic.Dictionary<string, string>() { { "testcase_name", "testaladdin1" } }, "folder", new System.Collections.Generic.List<string>() { { "D:/captureFrame_2022_5_1_23_10_50.zip" } }, (res, errorInfo) =>
+//        {
+//            Debug.Log($"传输结果:{res}  error:{errorInfo}");
+//        });
+//    }
+//}
 
-/*
+
 public class FileUpload : MonoBehaviour
 {
-    string url = "https://apigw-cn-south.huawei.com/api/cybersim/performance/unity/v1/energy/addUnityPerformanceInfo";
+    //string url = "https://apigw-cn-south.huawei.com/api/cybersim/performance/unity/v1/energy/addUnityPerformanceInfo";
     //string url = "http://124.223.54.98:888/TestHandler.ashx";
-    //string url = "http://192.168.0.110:8083/TestHandler.ashx";
+    string url = "http://192.168.0.110:8083/TestHandler.ashx";
     //public Button button_Test;//测试btn
     //public Text text;//提示txt
 
@@ -37,8 +37,12 @@ public class FileUpload : MonoBehaviour
     private void To_FileUpload()
     {
         WWWForm wWWForm = new WWWForm();
-        wWWForm.AddField("testcase_name", "testaladdin1");
-        wWWForm.AddBinaryData("folder", GetStreamBytes("D:/resMemoryDistribution_2022_5_1_23_10_50.txt"));
+        wWWForm.AddField("testcaseName", "testaladdin1");
+        //wWWForm.AddField("fileName", "captureFrame_2022_5_1_23_10_50.zip");
+        //wWWForm.AddField("fileName", "log_2022_05_31_20_54_38.txt");
+        wWWForm.AddField("fileName", "captureFrame_2022_5_1_23_10_51.rar");
+        wWWForm.AddBinaryData("folder", GetStreamBytes("D:/captureFrame_2022_5_1_23_10_51.rar"));
+        //wWWForm.AddBinaryData("folder", GetStreamBytes("D:/log_2022_05_31_20_54_38.txt"));
         //wWWForm.AddBinaryData("folder", GetStreamBytes("D:/captureFrame_2022_5_1_23_10_50.zip"));
 
         StartCoroutine(UploadResult(url, wWWForm));
@@ -74,7 +78,7 @@ public class FileUpload : MonoBehaviour
         {
             webRequest.SetRequestHeader("X-HW-ID", "com.huawei.xr.cyberverse.cybersim");
             webRequest.SetRequestHeader("X-HW-APPKEY", "bA2J8D1u9djyOVtS8efNTQ==");
-
+            webRequest.SetRequestHeader("Content-Encoding", "gzip");
             yield return webRequest.SendWebRequest();
 
             if (webRequest.result == UnityWebRequest.Result.Success)
@@ -90,4 +94,3 @@ public class FileUpload : MonoBehaviour
         }
     }
 }
-*/
